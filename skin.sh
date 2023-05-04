f364cef8c3052c2d4a1827fda5a765abb8a55da0
James Hu <49660809+jamesishandsome@users.noreply.github.com>
2023-05-04 13:12:39 +0000

    init

--- skin.sh
+++ skin.sh
@@ -1,14 +1,44 @@
 #!/bin/sh
+cd ~/dst/ugc_mods/MyDediServer/Caves/content/322330/1392778117/scripts
 rm skin_legion.lua
 wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/skin_legion.lua
+cd ~/dst/ugc_mods/MyDediServer/Master/content/322330/1392778117/scripts
+rm skin_legion.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/skin_legion.lua
+
+cd ~/dst/ugc_mods/MyDediServer/Caves/content/322330/1392778117/scripts
 rm apublicsupporter.lua
 wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/apublicsupporter.lua
-cd prefabs
+cd ~/dst/ugc_mods/MyDediServer/Master/content/322330/1392778117/scripts
+rm apublicsupporter.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/apublicsupporter.lua
+
+cd ~/dst/ugc_mods/MyDediServer/Caves/content/322330/1392778117/scripts/prefabs
 rm elecourmaline.lua
 wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/elecourmaline.lua
+cd ~/dst/ugc_mods/MyDediServer/Master/content/322330/1392778117/scripts/prefabs
+rm elecourmaline.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/elecourmaline.lua
+
+cd ~/dst/ugc_mods/MyDediServer/Caves/content/322330/1392778117/scripts/prefabs
 rm moon_dungeon.lua
 wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/moon_dungeon.lua
+cd ~/dst/ugc_mods/MyDediServer/Master/content/322330/1392778117/scripts/prefabs
+rm moon_dungeon.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/moon_dungeon.lua
+
+
+cd ~/dst/ugc_mods/MyDediServer/Caves/content/322330/1392778117/scripts/prefabs
 rm neverfade.lua
 wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/neverfade.lua
+cd ~/dst/ugc_mods/MyDediServer/Master/content/322330/1392778117/scripts/prefabs
+rm neverfade.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/neverfade.lua
+
+
+cd ~/dst/ugc_mods/MyDediServer/Caves/content/322330/1392778117/scripts/prefabs
+rm siving_rocks_legion.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/siving_rocks_legion.lua
+cd ~/dst/ugc_mods/MyDediServer/Master/content/322330/1392778117/scripts/prefabs
 rm siving_rocks_legion.lua
 wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/siving_rocks_legion.lua
