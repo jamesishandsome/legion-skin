c8b85724f8200dad640ac0acc8abf23d8c204665
James Hu <49660809+jamesishandsome@users.noreply.github.com>
2023-05-04 13:01:42 +0000

    init

---
+++ skin.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm skins_legion.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/skins_legion.lua
+rm apublicsupporter.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/apublicsupporter.lua
+cd prefabs
+rm elecourmaline.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/elecourmaline.lua
+rm moon_dungeon.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/moon_dungeon.lua
+rm neverfade.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/neverfade.lua
+rm siving_rocks_legion.lua
+wget https://raw.githubusercontent.com/jamesishandsome/legion-skin/master/siving_rocks_legion.lua
